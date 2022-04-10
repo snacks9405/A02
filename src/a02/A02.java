e000d126f0a6370c9aa538515a31d7ba21d28219
snacks9405 <93486382+snacks9405@users.noreply.github.com>
2022-04-09 22:09:06 -0500

    Final Masterpiece

--- src/a02/A02.java
+++ src/a02/A02.java
@@ -1,5 +1,15 @@
 package a02;
+/*
+Description: Assignment 2
+Author: alex rodriguez
+Date: 4.10.22
+Bugs: None that I know of
+Reflection: the biggest problem I had was with priorities. I was pulling my hair out
+trying to figure out why mine was different from yours. turns out I mistyped "no new job this slice" to
+"no new job slice". hours wasted. otherwise really enjoyed it. top 50% of your labs so far. 
 
+felt significantly easier than the last assignment spellchecker. 
+*/
 public class A02 {
     public static void main(String[] args) throws Exception {
         //Scheduler.testFIleParsing(); //prints array containing important values. left incase you wanted a peek.
